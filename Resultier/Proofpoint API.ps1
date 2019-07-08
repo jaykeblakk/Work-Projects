27152c320d5308edd3c4d5ce1edd8fc980305759
carterco <35984723+carterco@users.noreply.github.com>
2019-07-08 08:53:20 -0600

    Make API Endpoints idempotent

--- Resultier/Proofpoint API.ps1
+++ Resultier/Proofpoint API.ps1
@@ -1,9 +1,14 @@
+$Org = Read-Host "Organization"
 $Username = Read-Host "Username"
 $Pass = Read-Host "Password"
-$Uri = "https://us3.proofpointessentials.com/api/orgs/resultier.com/users"
 $ProofpointHeader = New-Object 'System.Collections.Generic.Dictionary[[String],[String]]'
 $ProofpointHeader.Add("X-User","$Username")
 $ProofpointHeader.Add("X-Password","$pass")
+$endpointuri = "https://us3.proofpointessentials.com/api/endpoint/$org"
+$endpointresults = Invoke-RestMethod -Uri $endpointuri -Headers $ProofpointHeader -Method GET
+$endpoint = $endpointresults.message.endpoints
+$Uri = "$endpoint/api/orgs/$org/users"
+$ProofpointHeader = New-Object 'System.Collections.Generic.Dictionary[[String],[String]]'
 $ProofpointHeader.Add("Content-Type","application/json")
 $users = Import-Csv "C:\AMD\test.csv" | ConvertTo-Json
 Invoke-RestMethod -uri $uri -Headers $ProofpointHeader -Method POST -Body $users -ContentType "application/json"
